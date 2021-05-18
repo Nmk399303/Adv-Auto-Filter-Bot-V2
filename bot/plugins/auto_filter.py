826593f2ce87ca3f4b253838f191cd70cb7e942d
Nmk399303 <78946350+Nmk399303@users.noreply.github.com>
2021-05-18 11:07:18 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -128,7 +128,7 @@ async def auto_filter(bot, update):
             )
         
     else:
-        reply_text("Please Check the spelling or the movie might not be available 🚫/ Not Released Yet ❌") 
+        await update.reply_text("Please Check the spelling or the movie might not be available 🚫/ Not Released Yet ❌") 
          
     
 
